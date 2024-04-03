4d73d07ad5aed097fdd513f19d123fd2c651c73e
ata-kaner <ata.kaner03@gmail.com>
2024-04-03 11:46:42 +0300

    Freecess & Fast Track: Remove from kernel
    Signed-off-by: ata-kaner <ata.kaner03@gmail.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -34,10 +34,6 @@
 #include "binder_alloc.h"
 #include "binder_trace.h"
 
-#ifdef CONFIG_SAMSUNG_FREECESS
-#include <linux/freecess.h>
-#endif
-
 #define MAX_ALLOCATION_SIZE (1024 * 1024)
 #define MAX_ASYNC_ALLOCATION_SIZE (512 * 1024)
 
@@ -433,19 +429,6 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 				alloc->pid, extra_buffers_size);
 		return ERR_PTR(-EINVAL);
 	}
-
-#ifdef CONFIG_SAMSUNG_FREECESS
-	if (is_async && (alloc->free_async_space < 3*(size + sizeof(struct binder_buffer))
-		|| (alloc->free_async_space < alloc->buffer_size/4))) {
-		struct task_struct *p;
-
-		rcu_read_lock();
-		p = find_task_by_vpid(alloc->pid);
-		rcu_read_unlock();
-		if (p && (thread_group_is_frozen(p) || p->jobctl & JOBCTL_TRAP_FREEZE))
-			binder_report(p, -1, "free_buffer_full", is_async);
-	}
-#endif
 
 	if (is_async &&
 	    alloc->free_async_space < size + sizeof(struct binder_buffer)) {
