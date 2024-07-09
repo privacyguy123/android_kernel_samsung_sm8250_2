5fe26365a081a49535780b26cca1388f276461f2
Dawid2849 <dawid2849@gmail.com>
2024-07-09 15:56:37 +0200

    fs: checkout path_umount to Linux 5.10
    Fixes umount in KernelSU
    
    Change-Id: I3c1ad7ca3e6c99dc1a91aed8d6d4e6e925719c23

--- fs/namespace.c
+++ fs/namespace.c
@@ -2065,8 +2065,6 @@ static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
 
-	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
-		return -EINVAL;
 	if (!may_mount())
 		return -EPERM;
 	if (path->dentry != path->mnt->mnt_root)
@@ -2080,6 +2078,7 @@ static int can_umount(const struct path *path, int flags)
 	return 0;
 }
 
+// caller is responsible for flags being sane
 int path_umount(struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
@@ -2094,6 +2093,7 @@ int path_umount(struct path *path, int flags)
 	mntput_no_expire(mnt);
 	return ret;
 }
+
 /*
  * Now umount can handle mount points as well as block devices.
  * This is important for filesystems which use unnamed block devices.
